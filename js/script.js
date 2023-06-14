6722e2d19825d2e86688d34763189e9e82f0db73
stefanenache12 <stefanenache12@gmail.com>
2023-06-14 15:43:37 +0200

    END MULTIPLO 3 E MULTIPLO 5

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let i = 1;
+
+for(i; i <= 100; i++) {
+
+    if(i % 3 === 0){
+        console.log('Fizz')
+    }
+    else if(i % 5 === 0){
+        console.log('Buzz')
+    }
+    else{
+        console.log(i)
+    }
+
+};
+
