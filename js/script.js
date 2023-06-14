f3ea50180c6bd8a29c1dfadd040e02c6b5b0b216
stefanenache12 <stefanenache12@gmail.com>
2023-06-14 16:10:00 +0200

    END CYCLE

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 let i = 1;
 
-for(i; i <= 100; i++) {
-
-    if(i % 3 === 0){
-        console.log('Fizz')
+for (i; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } 
+    else if (i % 3 === 0) {
+      console.log("Fizz");
+    } 
+    else if (i % 5 === 0) {
+      console.log("Buzz");
+    } 
+    else {
+      console.log(i);
     }
-    else if(i % 5 === 0){
-        console.log('Buzz')
-    }
-    else{
-        console.log(i)
-    }
-
-};
-
+  }
+  
